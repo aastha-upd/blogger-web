32f1d3ccc295eff1ec285df115c32138662709e3
Aastha Upadhyay <aasthaupadhyay@Aasthas-MacBook-Pro.local>
2024-02-18 20:39:08 +0530

    corrent env var

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 // Use process.env variable for the GraphQL server URI
-const GRAPHQL_URI = process.env.GRAPHQL_URI || 'https://blogger-service.onrender.com/graphql';
+const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI || 'https://blogger-service.onrender.com/graphql';
 
 const client = new ApolloClient({
     link: new HttpLink({
